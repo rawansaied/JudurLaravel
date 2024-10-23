50f3a1270255dd070d657282ab2d06d60e530891
“callmegemy” <callmegemy@gmail.com>
2024-10-23 12:57:37 +0300

    cors update

--- laraveljudur/config/cors.php
+++ laraveljudur/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:4200', 'https://bb6a-102-185-35-68.ngrok-free.app'],
+    'allowed_origins' => ['http://localhost:4200', 'https://323e-102-185-35-68.ngrok-free.app'],
 
     'allowed_origins_patterns' => [],
 
